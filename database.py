f9bdc69ccaac481595832c01b65576b629e787b5
theprojectall <theprojectall@outlook.com>
2023-07-30 16:25:32 -0500

    Pruebas unitarias

--- database.py
+++ database.py
@@ -1,35 +1,40 @@
 class Cliente:
+
     def __init__(self, dni, nombre, apellido):
         self.dni = dni
         self.nombre = nombre
         self.apellido = apellido
-        
+
     def __str__(self):
         return f"({self.dni}) {self.nombre} {self.apellido}"
-    
 class Clientes:
-    lista =[]
+
+    # Lista de clientes
+    lista = []
+
     @staticmethod
     def buscar(dni):
         for cliente in Clientes.lista:
             if cliente.dni == dni:
                 return cliente
+
     @staticmethod
     def crear(dni, nombre, apellido):
         cliente = Cliente(dni, nombre, apellido)
         Clientes.lista.append(cliente)
         return cliente
+
     @staticmethod
     def modificar(dni, nombre, apellido):
-        for indice,cliente in enumerate(Clientes.lista):
-            if cliente.dni ==dni:
-                Clientes.lista[indice].nombre = nombre
-                Clientes.lista[indice].apellido = apellido
-                return Cliente.lista[indice]
-            
+        for i, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                Clientes.lista[i].nombre = nombre
+                Clientes.lista[i].apellido = apellido
+                return Clientes.lista[i]
+
     @staticmethod
     def borrar(dni):
-        for indice, cliente in enumerate(Cliente.lista):
-            if cliente.dni==dni:
-                return Clientes.lista.pop[indice]
-    
+        for i, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                cliente = Clientes.lista.pop(i)
+                return cliente
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,15 +1,37 @@
+import copy
+import sys
+sys.path.append("/Users/theprojectall/Desktop/Personal/Proyecto Final Python")
 import unittest
 import database as db
 
 class TestDatabase(unittest.TestCase):
     def setUp(self):
         db.Clientes.lista = [
-            db.cliente('15J','Marta','Perez'),
-            db.cliente('48H','Manolo','López'),
-            db.cliente('28Z','Ana','Garcia')
+            db.Cliente('15J', 'Marta', 'Perez'),
+            db.Cliente('48H', 'Manolo', 'López'),
+            db.Cliente('28Z', 'Ana', 'Garcia')
         ]
-        def text_buscar_cliente(self):
-            cliente_extistente = db.Clientes.buscar('15J')
-            cliente_inextistente = db.Clientes.buscar('99X')
-            self.assertIsNotNone(cliente_extistente)
-            self.assertIsNone(cliente_inextistente)
+
+    def test_buscar_cliente(self):
+        cliente_existente = db.Clientes.buscar('15J')
+        cliente_inexistente = db.Clientes.buscar('99X')
+        self.assertIsNotNone(cliente_existente)
+        self.assertIsNone(cliente_inexistente)
+
+    def test_crear_cliente(self):
+        nuevo_cliente = db.Clientes.crear('39X', 'Héctor', 'Costa')
+        self.assertEqual(len(db.Clientes.lista), 4)
+        self.assertEqual(nuevo_cliente.dni, '39X')
+        self.assertEqual(nuevo_cliente.nombre, 'Héctor')
+        self.assertEqual(nuevo_cliente.apellido, 'Costa')
+
+    def test_modificar_cliente(self):
+        cliente_a_modificar = copy.copy(db.Clientes.buscar('28Z'))
+        cliente_modificado = db.Clientes.modificar('28Z', 'Mariana', 'Pérez')
+        self.assertEqual(cliente_a_modificar.nombre, 'Ana')
+        self.assertEqual(cliente_modificado.nombre, 'Mariana')
+
+    def test_borrar_cliente(self):
+        cliente_borrado = db.Clientes.borrar('48H')
+        cliente_rebuscado = db.Clientes.buscar('48H')
+        self.assertNotEqual(cliente_borrado, cliente_rebuscado)
