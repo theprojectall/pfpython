823638dbe23b95fc74c142b4341173d3d862ce29
theprojectall <theprojectall@outlook.com>
2023-07-30 17:09:52 -0500

    Se añadé expresión regular para la validación de DNI y sus pruebas unitarias correspondientes

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append("/Users/theprojectall/Desktop/Personal/Proyecto Final Python")
 import unittest
 import database as db
+import helpers
 
 class TestDatabase(unittest.TestCase):
     def setUp(self):
@@ -35,3 +36,9 @@ class TestDatabase(unittest.TestCase):
         cliente_borrado = db.Clientes.borrar('48H')
         cliente_rebuscado = db.Clientes.buscar('48H')
         self.assertNotEqual(cliente_borrado, cliente_rebuscado)
+        
+    def test_dni_valido(self):
+        self.assertTrue(helpers.dni_valido('00A', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('23223S', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('F35', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('48H', db.Clientes.lista))
