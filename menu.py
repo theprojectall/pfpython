858f694a6695d869768cc1eb939d42ad2c6da212
theprojectall <theprojectall@outlook.com>
2023-07-30 18:39:47 -0500

    Añadiendo validaciones adicionales

--- menu.py
+++ menu.py
@@ -29,17 +29,22 @@ def iniciar():
             dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
             cliente = db.Clientes.buscar(dni)
             print(cliente) if cliente else print("Cliente no encontrado.")
+            
         elif opcion == '3':
-            print("Añadiendo un cliente...\n")
-            dni = helpers.leer_texto(
-                3, 3, "DNI (2 ints y 1 char)").upper()
-            nombre = helpers.leer_texto(
-                2, 30, "Nombre (de 2 a 30 chars)").capitalize()
-            apellido = helpers.leer_texto(
-                2, 30, "Apellido (de 2 a 30 chars)").capitalize()
-            db.Clientes.crear(dni, nombre, apellido)
-            print("Cliente añadido correctamente.")
-    
+                print("Añadiendo un cliente...\n")
+                
+                dni = None
+                # Comprobación de DNI válido
+                while True:
+                    dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
+                    if helpers.dni_valido(dni, db.Clientes.lista):
+                        break
+                    
+                nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()
+                apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()
+                db.Clientes.crear(dni, nombre, apellido)
+                print("Cliente añadido correctamente.")
+
         elif opcion == '4':
             print("Modificando un cliente...\n")
             dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
