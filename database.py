28d7e907bea76cd76ff853cd8c7c694ef7b0e42e
theprojectall <theprojectall@outlook.com>
2023-07-30 20:15:52 -0500

    Configuraciones de la UI del aplicativo

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 import csv
 import config
 
-class Cliente:
 
+class Cliente:
     def __init__(self, dni, nombre, apellido):
         self.dni = dni
         self.nombre = nombre
@@ -10,12 +10,16 @@ class Cliente:
 
     def __str__(self):
         return f"({self.dni}) {self.nombre} {self.apellido}"
+
+    def to_dict(self):
+        return {'dni': self.dni, 'nombre': self.nombre, 'apellido': self.apellido}
+
+
 class Clientes:
 
-    # Lista de clientes
     lista = []
-    with open(config.DATABASE_PATH, newline="\n") as fichero:
-        reader = csv.reader(fichero, delimiter=";" )
+    with open(config.DATABASE_PATH, newline='\n') as fichero:
+        reader = csv.reader(fichero, delimiter=';')
         for dni, nombre, apellido in reader:
             cliente = Cliente(dni, nombre, apellido)
             lista.append(cliente)
@@ -35,24 +39,24 @@ class Clientes:
 
     @staticmethod
     def modificar(dni, nombre, apellido):
-        for i, cliente in enumerate(Clientes.lista):
+        for indice, cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
-                Clientes.lista[i].nombre = nombre
-                Clientes.lista[i].apellido = apellido
+                Clientes.lista[indice].nombre = nombre
+                Clientes.lista[indice].apellido = apellido
                 Clientes.guardar()
-                return Clientes.lista[i]
+                return Clientes.lista[indice]
 
     @staticmethod
     def borrar(dni):
-        for i, cliente in enumerate(Clientes.lista):
+        for indice, cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
-                cliente = Clientes.lista.pop(i)
+                cliente = Clientes.lista.pop(indice)
                 Clientes.guardar()
                 return cliente
-    
+
     @staticmethod
     def guardar():
-        with open(config.DATABASE_PATH, "w", newline="\n") as fichero:
-         writer = csv.writer(fichero, delimiter=";")
-         for c in Clientes.lista:
-            writer.writerow((c.dni, c.nombre, c.apellido))
+        with open(config.DATABASE_PATH, 'w', newline='\n') as fichero:
+            writer = csv.writer(fichero, delimiter=';')
+            for cliente in Clientes.lista:
+                writer.writerow((cliente.dni, cliente.nombre, cliente.apellido))
